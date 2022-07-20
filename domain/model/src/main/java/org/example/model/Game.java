5b0006ef91f35aceb61f47351fd498498890514a
emanuelvallejoc <emanuel.vallejo@sofka.com.co>
2022-07-20 15:39:54 -0500

    se agrega game

---
+++ domain/model/src/main/java/org/example/model/Game.java
@@ -0,0 +1,42 @@
+package org.example.model;
+
+import java.util.Set;
+
+public class Game {
+
+    private String id;
+
+    private Set<Player> players;
+
+    public Game() {
+    }
+
+    public Game(String id, Set<Player> players) {
+        this.id = id;
+        this.players = players;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Set<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Set<Player> players) {
+        this.players = players;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "id='" + id + '\'' +
+                ", players=" + players +
+                '}';
+    }
+}
