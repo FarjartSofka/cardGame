459c34bf3ee0e11184b47805a5652626340a46c4
Farly Artunduaga <farly.artunduaga@sofka.com.co>
2022-07-20 12:22:24 -0500

    Gestion de conexion atlas en la nube y repositorio de Card

--- applications/src/main/java/org/example/config/MongoDBSecret.java
+++ applications/src/main/java/org/example/config/MongoDBSecret.java
@@ -1,4 +1,15 @@
 package org.example.config;
 
 public class MongoDBSecret {
+
+    private final String uri;
+
+    public MongoDBSecret(String uri) {
+        this.uri = uri;
+    }
+
+    public String getUri() {
+        return this.uri;
+    }
+
 }
