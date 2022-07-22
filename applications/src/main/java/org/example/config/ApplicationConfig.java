192f490db7c9c9857f7977cdd534224b21603eed
Farly Artunduaga <farly.artunduaga@sofka.com.co>
2022-07-22 17:58:21 -0500

    Inclusion de dependencia de RabbitMQ

--- applications/src/main/java/org/example/config/ApplicationConfig.java
+++ applications/src/main/java/org/example/config/ApplicationConfig.java
@@ -1,6 +1,13 @@
 package org.example.config;
 
+import co.com.sofka.business.generic.ServiceBuilder;
+import co.com.sofka.infraestructure.asyn.SubscriberEvent;
+import co.com.sofka.infraestructure.bus.EventBus;
+import co.com.sofka.infraestructure.repository.EventStoreRepository;
 import org.reactivecommons.utils.ObjectMapperI;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.boot.autoconfigure.mongo.MongoPropertiesClientSettingsBuilderCustomizer;
@@ -15,14 +22,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
-@ComponentScan(basePackages = {"org.example.usecase", "org.example.model", "org.example.adapters"},
+@ComponentScan(basePackages = {"org.example.cardGame.usecase", "org.example.cardGame.model", "org.example.cardGame.adapters",
+        "org.example.cardGame.controller", "org.example.cardGame.handler", "org.example.cardGame.event"},
         includeFilters = {
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$"),
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+Repository$"),
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+Adapter$"),
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+Controller$"),
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+Handler$"),
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+Event$"),
         },
         useDefaultFilters = false)
 public class ApplicationConfig {
+
+    public static final String EXCHANGE = "cardGame";
 
     @Bean
     public MongoDBSecret dbSecret(Environment env) {
@@ -42,5 +55,24 @@ public class ApplicationConfig {
     @Bean
     public ObjectMapperI objectMapperI() {
         return new ObjectMapperI();
+    }
+
+    @Bean
+    public SubscriberEvent subscriberEvent(EventStoreRepository eventStoreRepository, EventBus eventBus) {
+        return new SubscriberEvent(eventStoreRepository, eventBus);
+    }
+
+    @Bean
+    public ServiceBuilder serviceBuilder(
+    ) {
+        ServiceBuilder serviceBuilder = new ServiceBuilder();
+        return serviceBuilder;
+    }
+
+    @Bean
+    public RabbitAdmin rabbitmqAdmin(RabbitTemplate rabbitmqTemplate) {
+        var admin = new RabbitAdmin(rabbitmqTemplate);
+        admin.declareExchange(new TopicExchange(EXCHANGE));
+        return admin;
     }
 }
