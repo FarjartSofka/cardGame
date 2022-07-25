2496ac7f2cbce708efdf0f3ad943fb083977940e
emanuelvallejoc <emanuel.vallejo@sofka.com.co>
2022-07-24 23:23:08 -0500

    Update WebFluxConfig.java

--- applications/src/main/java/org/example/config/WebFluxConfig.java
+++ applications/src/main/java/org/example/config/WebFluxConfig.java
@@ -2,6 +2,10 @@ package org.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
@@ -9,14 +13,16 @@ import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
 
 @Configuration
 @EnableWebFlux
-public class WebFluxConfig implements WebFluxConfigurer {
-
+public class WebFluxConfig {
     @Bean
-    public  WebFluxConfigurer corsConfigure() {
-        return new WebFluxConfigurerComposite(){
-            public void addCorsMapping(CorsRegistry registry){
-                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+    public WebFluxConfigurer corsConfigurer() {
+        return new WebFluxConfigurerComposite() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*")
+                        .allowedMethods("*");
             }
         };
     }
+
 }
